886d35ab70e05340661630662af4ab25ecded71e
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2017-01-30 18:06:46 -0300

    add files

---
+++ app.py
@@ -0,0 +1,18 @@
+import socket
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Running at {0}".format(request.host)
+
+
+if __name__ == "__main__":
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost', 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    app.run(port=port)
+    app.run(debug=True, host='0.0.0.0')
