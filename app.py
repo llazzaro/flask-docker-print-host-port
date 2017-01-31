f8a14493abb0a1186528cbd8aaf3cfb18c4287b2
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2017-01-31 12:21:02 -0300

    add APP_ID

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+from random import randint
 from flask import Flask, request
 
 app = Flask(__name__)
-
+APP_ID = randint(0, 10000000)
 
 @app.route("/")
 def hello():
-    return "Running at {0}".format(request.host)
+    return "Running at {0}. APP_ID={1}".format(request.host, APP_ID)
 
 
 if __name__ == "__main__":
