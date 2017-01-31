2eb2881334c861bcbda34b8ef72efaaefbe75853
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2017-01-31 12:01:37 -0300

    clean up

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import socket
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -10,8 +9,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('0.0.0.0', 0))
-    port = sock.getsockname()[1]
-    sock.close()
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=5000)
